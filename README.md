367b7db6f6c40e7b698c4602485ffaae2556c095
setsav <jwhittwork@tuta.io>
2024-12-28 18:11:36 -0500

    updated readme and tweaked colors for more variety and accuracy

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # 77colors.nvim
-A Neovim colorscheme to match Cyberpunk 2077's hacking minigame
+A Neovim colorscheme to match Cyberpunk 2077's hacking minigame.
+
+## Installation
+
+Using lazy:
+
+```lua
+{
+    "setsav/77colors.nvim",
+    opts = {
+        transparent = true -- True or false
+    }
+},
+```
+
+## Set color scheme
+
+```
+:colorscheme 77colors
+```
+
+or:
+
+```lua
+vim.cmd("colorscheme 77colors")
+```
+
+## Lualine
+
+To enable 77colors for lualine, make sure it is defined in your lualine config. e.g.:
+
+```lua
+{
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = '77colors',
+        },
+      },
+    })
+  end,
+},
+```
--- lua/lualine/themes/77colors.lua
+++ lua/lualine/themes/77colors.lua
@@ -1,36 +1,40 @@
 local colors = require('77colors').colors
 
+if not require('77colors').transparent then
+  colors.tp = colors.black
+end
+
 local llcolors = {}
 
-  llcolors.normal = {
-    a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.yellow, fg = colors.black },
-    c = { bg = colors.tp, fg = colors.white }
-  }
-  llcolors.insert = {
-    a = { bg = colors.green, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.green, fg = colors.black },
-    c = { bg = colors.tp, fg = colors.white }
-  }
-  llcolors.visual = {
-    a = { bg = colors.cyan, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.cyan, fg = colors.black },
-    c = { bg = colors.tp, fg = colors.white }
-  }
-  llcolors.replace = {
-    a = { bg = colors.red, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.red, fg = colors.black },
-    c = { bg = colors.tp, fg = colors.white }
-  }
-  llcolors.command = {
-    a = { bg = colors.red, fg = colors.cyan, gui = 'bold' },
-    b = { bg = colors.red, fg = colors.cyan },
-    c = { bg = colors.tp, fg = colors.white }
-  }
-  llcolors.inactive = {
-    a = { bg = colors.fg, fg = colors.gray, gui = 'bold' },
-    b = { bg = colors.fg, fg = colors.gray },
-    c = { bg = colors.tp, fg = colors.white }
-  }
+llcolors.normal = {
+  a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.yellow, fg = colors.black },
+  c = { bg = colors.tp, fg = colors.cyan }
+}
+llcolors.insert = {
+  a = { bg = colors.green, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.green, fg = colors.black },
+  c = { bg = colors.tp, fg = colors.cyan }
+}
+llcolors.visual = {
+  a = { bg = colors.cyan, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.cyan, fg = colors.black },
+  c = { bg = colors.tp, fg = colors.cyan }
+}
+llcolors.replace = {
+  a = { bg = colors.red, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.red, fg = colors.black },
+  c = { bg = colors.tp, fg = colors.cyan }
+}
+llcolors.command = {
+  a = { bg = colors.red, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.red, fg = colors.black },
+  c = { bg = colors.tp, fg = colors.cyan }
+}
+llcolors.inactive = {
+  a = { bg = colors.fg, fg = colors.gray, gui = 'bold' },
+  b = { bg = colors.fg, fg = colors.gray },
+  c = { bg = colors.tp, fg = colors.white }
+}
 
 return llcolors
